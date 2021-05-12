3ae991b13a6f1b6879c4d4e730827d36c12ec9e1
NicolasGarcia2 <38087848+nikarcia@users.noreply.github.com>
2021-05-11 22:24:31 -0500

    Update web_service2.py

--- web_service2.py
+++ web_service2.py
@@ -3,79 +3,21 @@ import time
 
 from flask import Flask, request, abort, jsonify, send_from_directory, redirect, url_for
 
+
 UPLOAD_DIRECTORY = "."
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
-api = Flask(_name_)
-api.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+api = Flask(__name__)
 
-
-@api.route("/voice" , methods=[ 'POST'])
-def voice():
-    content = request.json
-    print (content['out_name'])
-    print (content['lyrics'])
-    print (content['midi'])
-    print (content['sex'])
-    print (content['tempo'])
-    print (content['method'])
-    print (content['language'])
-
-
-    return "respuesta desde integracion"
-
-@api.route("/files" , methods = [ 'GET'])
-def list_files():
+@api.route("/prueba" , methods=[ 'POST'])
+def prueba():
+    content=request.json
+    print(content['hola'])
     """Endpoint to list files on the server."""
-    files = []
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        path = os.path.join(UPLOAD_DIRECTORY, filename)
-        if os.path.isfile(path):
-            files.append(filename)
-    return jsonify(files)
-  @api.route("/files/<path:path>")
-def get_file(path):
-    """Download a file."""
-    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
+    return "hola"
 
-@api.route("/files/<filename>", methods=["POST"])
-def post_file(filename):
-    """Upload a file."""
-    if "/" in filename:
-        # Return 400 BAD REQUEST
-        abort(400, "no subdirectories allowed")
-
-    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-        fp.write(request.data)
-
-    # Return 201 CREATED
-    return "", 201
-  UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-api.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-  # this has changed from the original example because the original did not work for me
-    return filename[-3:].lower() in ALLOWED_EXTENSIONS
-
-@api.route('/', methods=[ 'POST'])
-def upload_file():
-        file = request.files['file']
-        print ('**found file', file.filename)
-        #filename = secure_filename(file.filename)
-        file.save(os.path.join(api.config['UPLOAD_FOLDER'], file.filename))
-        # for browser, add 'redirect' function on top of 'url_for'
-        return url_for('uploaded_file', filename=file.filename)
-
-@api.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(api.config['UPLOAD_FOLDER'],filename)
-
-
-if _name_ == "_main_":
-    api.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    api.run(debug=True, port=8000)
